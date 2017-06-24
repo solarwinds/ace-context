46e664f8f3771096c6348e44a40ffcf2f7058775
Jeff Lewis <jlewis@streetconnect.com>
2017-06-24 02:42:02 -0400

    test: skip uncaughtException test

--- test/tap/proper-exit.tap.js
+++ test/tap/proper-exit.tap.js
@@ -1,14 +1,22 @@
 'use strict';
+var tap = require('tap');
+var test = tap.test;
+var util = require('util');
 
-process.on('uncaughtException', function (err) {
-  if (err.message === 'oops') {
-    console.log("ok got expected message: %s", err.message);
-  }
-  else {
-    throw err;
-  }
+test('proper exit on uncaughtException', {skip: true}, function(t) {
+  process.on('uncaughtException', function(err) {
+    if (err.message === 'oops') {
+      //console.log("ok got expected message: %s", err.message);
+      t.pass(util.format("ok got expected message: %s", err.message));
+    }
+    else {
+      throw err;
+    }
+  });
+
+  var cls = require('../../context.js');
+  var ns = cls.createNamespace('x');
+  ns.run(function() {
+    throw new Error('oops');
+  });
 });
-
-var cls = require('../../context.js');
-var ns = cls.createNamespace('x');
-ns.run(function () { throw new Error('oops'); });
